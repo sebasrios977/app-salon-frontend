15bcc59514dee08e1c6d1f7b9c12b9ba623e593c
sebasrios97 <sebastianriosrico@gmail.com>
2025-10-15 18:06:04 -0500

    42th section completed

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -4,5 +4,8 @@ import api from "@/lib/axios";
 export default {
     register(data) {
         return api.post("/auth/register", data);
-    }
+    },
+    confirmAccount(token) {
+        return api.get(`/auth/verify/${token}`);
+    },
 }
--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,9 +1,29 @@
-<script setup></script>
+<script setup>
+import { inject, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import AuthAPI from "@/api/AuthAPI";
+
+const route = useRoute();
+const router = useRouter();
+const { token } = route.params;
+
+const toast = inject("toast");
+
+onMounted(async () => {
+  try {
+    const { data } = await AuthAPI.confirmAccount(token);
+    toast.success(data.msg);
+    setTimeout(() => {
+      router.push({ name: "login" });
+    }, 3000);
+  } catch (error) {
+    toast.error(error.response.data.msg);
+  }
+});
+</script>
 
 <template>
-  <div>
-    <h1>Desde Confirm Account View</h1>
-  </div>
+  <h1 class="text-6xl font-extrabold text-white text-center mt-10">
+    Verificar Cuenta
+  </h1>
 </template>
-
-
